00d2b839abd6250b3e8a5639a0ae01ec6d8855f1
Jim <jim.pollaro@gmail.com>
2017-09-15 16:16:38 -0700

    Turned in version

--- main/apps/randomWord/urls.py
+++ main/apps/randomWord/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views           # This line is new!
 urlpatterns = [
     url(r'^$',views.index),
-    url(r'^reset/',views.reset)
+    url(r'^reset/',views.reset),
+    url(r'^generate/$',views.generate)
 ]
--- main/apps/randomWord/views.py
+++ main/apps/randomWord/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, redirect, HttpResponse
-# from django.utils.crypto import get_random_string
+from django.utils.crypto import get_random_string
 
 def index(request):
     word = {'word':get_random_string(length=14)}
-    return render(request,'index.html',word)
+    if 'count' not in request.session or request.session['count'] == 0:
+        request.session['count'] = 1
+    return render(request,'randomWord/index.html',word)
+
+def generate(request):
+    if request.method == 'POST':
+        request.session['count'] += 1
+    return redirect(index)
 
 def reset(request):
-
+    request.session.clear()
     return redirect(index)
